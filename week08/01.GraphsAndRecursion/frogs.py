f9666301068002fcd9094007ba2246189377e679
TsvetomirTsvetkov <cvetomircvetkov24@gmail.com>
2020-04-26 20:00:09 +0300

    Refactored code for frogs.py

--- week08/01.GraphsAndRecursion/frogs.py
+++ week08/01.GraphsAndRecursion/frogs.py
@@ -18,10 +18,10 @@ def lists_to_strings(lists_to_convert):
         print(helper)
 
 
-def change_frogs_positions(lilypads, old_index, new_index):
-    tmp = lilypads[old_index]
-    lilypads[old_index] = lilypads[new_index]
-    lilypads[new_index] = tmp
+def change_frogs_positions(lilypads, old_index_free_lilypad, new_index_free_lilypad):
+    tmp = lilypads[old_index_free_lilypad]
+    lilypads[old_index_free_lilypad] = lilypads[new_index_free_lilypad]
+    lilypads[new_index_free_lilypad] = tmp
     return lilypads
 
 
@@ -29,55 +29,38 @@ def frogs(number_of_lilypads):
     assert number_of_lilypads % 2 != 0 and number_of_lilypads >= 3, 'Lilypads must be an odd number >= 3.'
     beginning = list(create_swamp(number_of_lilypads, '>', '<'))
     end = list(create_swamp(number_of_lilypads, '<', '>'))
-    index = number_of_lilypads // 2
-    helper = [beginning[:]]
+    index_free_lilypad = number_of_lilypads // 2
+    jumps_list = [beginning[:]]
 
-    def find_path(beginning, end, index, helper):
+    def recursive_call(beginning, end, index_free_lilypad, jumps_list, new_index_free_lilypad):
+        beginning = change_frogs_positions(beginning, index_free_lilypad, new_index_free_lilypad)
+        hold_index = index_free_lilypad
+        index_free_lilypad = new_index_free_lilypad
+        if beginning not in jumps_list:
+                jumps_list.append(beginning)
+        find_path(beginning[:], end, index_free_lilypad, jumps_list)
+        jumps_list.pop()
+        beginning = change_frogs_positions(beginning, index_free_lilypad, hold_index)
+        index_free_lilypad = hold_index
+
+    def find_path(beginning, end, index_free_lilypad, jumps_list):
         if beginning == end:
-            lists_to_strings(helper)
+            lists_to_strings(jumps_list)
             print(len(beginning) * '=')
             return
 
-        if index - 1 >= 0 and beginning[index - 1] == '>':
-            beginning = change_frogs_positions(beginning, index, index - 1)
-            index -= 1
-            if beginning not in helper:
-                helper.append(beginning)
-            find_path(beginning[:], end, index, helper)
-            helper.pop()
-            beginning = change_frogs_positions(beginning, index, index + 1)
-            index += 1
+        if index_free_lilypad - 1 >= 0 and beginning[index_free_lilypad - 1] == '>':
+            recursive_call(beginning, end, index_free_lilypad, jumps_list, index_free_lilypad - 1)
 
-        if index - 2 >= 0 and beginning[index - 2] == '>':
-            beginning = change_frogs_positions(beginning, index, index - 2)
-            index -= 2
-            if beginning not in helper:
-                helper.append(beginning)
-            find_path(beginning[:], end, index, helper)
-            helper.pop()
-            beginning = change_frogs_positions(beginning, index, index + 2)
-            index += 2
+        if index_free_lilypad - 2 >= 0 and beginning[index_free_lilypad - 2] == '>':
+            recursive_call(beginning, end, index_free_lilypad, jumps_list, index_free_lilypad - 2)
 
-        if index + 1 < len(beginning) and beginning[index + 1] == '<':
-            beginning = change_frogs_positions(beginning, index, index + 1)
-            index += 1
-            if beginning not in helper:
-                helper.append(beginning)
-            find_path(beginning[:], end, index, helper)
-            helper.pop()
-            beginning = change_frogs_positions(beginning, index, index - 1)
-            index -= 1
+        if index_free_lilypad + 1 < len(beginning) and beginning[index_free_lilypad + 1] == '<':
+            recursive_call(beginning, end, index_free_lilypad, jumps_list, index_free_lilypad + 1)
 
-        if index + 2 < len(beginning) and beginning[index + 2] == '<':
-            beginning = change_frogs_positions(beginning, index, index + 2)
-            index += 2
-            if beginning not in helper:
-                helper.append(beginning)
-            find_path(beginning[:], end, index, helper)
-            helper.pop()
-            beginning = change_frogs_positions(beginning, index, index - 2)
-            index -= 2
-    find_path(beginning, end, index, helper)
+        if index_free_lilypad + 2 < len(beginning) and beginning[index_free_lilypad + 2] == '<':
+            recursive_call(beginning, end, index_free_lilypad, jumps_list, index_free_lilypad + 2)
+    find_path(beginning, end, index_free_lilypad, jumps_list)
 
 
 def main():
